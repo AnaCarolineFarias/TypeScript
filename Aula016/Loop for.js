9760362b07a4ab8f5cac84bfb1d599e2b1d700c6
Ana Caroline de Oliveira Farias <anacarolinedeoliveirafarias@gmail.com>
2024-11-13 10:00:26 -0300

    Create Loop for.js

---
+++ Aula016/Loop for.js
@@ -0,0 +1,25 @@
+"use strict";
+/*
+* Arquivo: index.ts
+* Descrição: Arquivo responsável por praticar conceitos básicos sobre 'For'
+* Author: Ana Caroline <@_fariaasz@>
+* Data: 13/11/2024
+* Transpilação do arquivo: <Windows> CTRL + SHIFT + B -> tsc: build/watch
+*/
+// -> Exemplo 01 - Loop For
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+// -> Exemplo 02 - For...Of (Retorna uma coleção de matriz, lista ou tupla)
+const arrayNumeros = [10, 20, 30, 40, 50];
+for (const i of arrayNumeros) {
+    console.log(i);
+}
+// -> Exemplo 03 - For...In ()
+const arrayNumeros_01 = [5, 4, 3, 2, 1, 0];
+for (const i in arrayNumeros_01) {
+    console.log(i);
+}
+// sort()
+const ordemNumerica = arrayNumeros_01.sort();
+console.log(ordemNumerica);
